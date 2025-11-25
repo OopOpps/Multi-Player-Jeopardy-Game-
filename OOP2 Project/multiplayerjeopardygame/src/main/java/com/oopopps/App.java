2cd6f7cf37390e03602cb60e62893d43e77e53e7
kwah2001 <114884885+kwah2001@users.noreply.github.com>
2025-11-24 23:21:30 -0400

    Update App.java

--- OOP2 Project/multiplayerjeopardygame/src/main/java/com/oopopps/App.java
+++ OOP2 Project/multiplayerjeopardygame/src/main/java/com/oopopps/App.java
@@ -1,13 +1,42 @@
 package com.oopopps;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to UWI Jeopardy Game");
+        Scanner inputScanner = new Scanner(System.in);
+        
+        System.out.println("Please type the path to your question file (XML, JSON, or CSV)");
+        System.out.println("Or just press Enter to use the default file: ");
+        String userInput = inputScanner.nextLine();
+        
+        Path filePath;
+        
+        if (userInput.isEmpty()) {
+            filePath = Paths.get("src/main/resources/sample_game_XML.xml");
+            System.out.println("Using default file: " + filePath);
+        } 
+        else {
+            filePath = Paths.get(userInput);
+            System.out.println("Using your file: " + filePath);
+        }
+        
+        try {
+            GameEngine game = new GameEngine(filePath);
+            game.run();
+        } 
+        catch (Exception error) {
+            System.out.println("Sorry, something went wrong!");
+            System.out.println("Error: " + error.getMessage());
+            error.printStackTrace();
+        } 
+        finally {
+            inputScanner.close();
+            System.out.println("Game ended. Thanks for playing!");
+        }
     }
 }
+
