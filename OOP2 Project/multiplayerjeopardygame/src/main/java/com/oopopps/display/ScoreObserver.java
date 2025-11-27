a55f599f997e64464d67e859d96f848f9fa2caa5
kwah2001 <114884885+kwah2001@users.noreply.github.com>
2025-11-26 21:53:51 -0400

    Update ScoreObserver.java

--- OOP2 Project/multiplayerjeopardygame/src/main/java/com/oopopps/display/ScoreObserver.java
+++ OOP2 Project/multiplayerjeopardygame/src/main/java/com/oopopps/display/ScoreObserver.java
@@ -1,6 +1,16 @@
 package com.oopopps.display;
 import com.oopopps.Player;
 
+/**
+ * Defines the contract for objects that need to be notified when player scores change.
+ * Implements the Observer pattern for score tracking.
+ */
 public interface ScoreObserver {
+    
+    /**
+     * Called when a player's score has been updated.
+     * 
+     * @param p the player whose score has changed
+     */
     void update(Player p);
 }
